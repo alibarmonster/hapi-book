1d55a7e698f0b7d1ccaffcc1f04ed7de2cd675f0
alibarmonster <alibar838@gmail.com>
2023-03-22 17:24:09 +0700

    add Dockerfile

--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,37 @@
-const { addBookHandler, getAllBookHandler, getBookByIdHandler, updateBookIdHandler, deleteBookHandler } = require('./handler');
+const {
+  addBookHandler,
+  getAllBookHandler,
+  getBookByIdHandler,
+  updateBookIdHandler,
+  deleteBookHandler,
+} = require('./handler');
 
 const routes = [
-    {
-        method: 'POST',
-        path: '/books',
-        handler: addBookHandler,
-    },
-    {
-        method: 'GET',
-        path: '/books',
-        handler: getAllBookHandler,
-    },
-    {
-        method: 'GET',
-        path: '/books/{id}',
-        handler: getBookByIdHandler,
-    },
-    {
-        method: 'PUT',
-        path: '/books/{id}',
-        handler: updateBookIdHandler,
-    },
-    {
-        method: 'DELETE',
-        path: '/books/{id}',
-        handler: deleteBookHandler,
-    },
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: updateBookIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookHandler,
+  },
 ];
 
 module.exports = routes;
